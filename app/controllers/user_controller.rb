ba266aaf4d44f650ce126516846978c35749151b
i-kaz42 <gurant564@gmail.com>
2020-12-11 16:27:38 +0900

    twitter_cloneapp

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @User = User.find(params[:id])
+    @tweets = @user.tweets
+  end
+end
