80fb5d9ce2c9e89df5b019c95ecbd0ab613070a0
i-kaz42 <gurant564@gmail.com>
2020-12-17 17:30:51 +0900

    add_bootstrap

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,4 +8,14 @@ class UserController < ApplicationController
     @User = User.find(params[:id])
     @tweets = @user.tweets
   end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
 end
